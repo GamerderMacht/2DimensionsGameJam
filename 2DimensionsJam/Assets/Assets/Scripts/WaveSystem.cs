efe0fa2db182bbb6c9c25bbff4827122cfe1cfcc
Jabari Belgrave <JBelgrave@my.harrisburgu.edu>
2023-05-28 17:12:43 -0400

    Finished adding the enemy prefabs as well as the weapon prefabs to spawn in each wave. Also added sound to the gun when the player shoots and sound when the bomb goes off.

--- 2DimensionsJam/Assets/Assets/Scripts/WaveSystem.cs
+++ 2DimensionsJam/Assets/Assets/Scripts/WaveSystem.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class WaveSystem : MonoBehaviour
 {
     [Header("List of enemies alive")]
-    public List<GameObject> alivelist = new List<GameObject>();
+    public List<GameObject> aliveList = new List<GameObject>();
 
+    [Space]
+    
     [Header("Wave Timer")]
     [SerializeField] float nextWaveStart;
-
-    [Header("Wave")]
-    public Transform[] SpawnPoints;
+    
+    [Space]
+    
+    [Header("Weapon Spawning")]
+    public Transform[] WeaponSpawnPoints;
+    public List<GameObject> weaponList = new List<GameObject>();
+    
+    [Space]
+    
+    [Header("Enemy Wave Spawning")]
+    public Transform[] EnemySpawnPoints;
     [SerializeField] List<WaveConfigSO> waveConfigs;
     WaveConfigSO currentWave;
     public bool canSpawn;
@@ -30,20 +40,28 @@ public class WaveSystem : MonoBehaviour
             {
                 if (canSpawn)
                 {
-                    var randomSpawnPoint = SpawnPoints[Random.Range(0, SpawnPoints.Length - 1)];
+                    var randomEnemySpawnPoint = EnemySpawnPoints[Random.Range(0, EnemySpawnPoints.Length - 1)];
                     GameObject enemy;
-                    enemy = Instantiate(currentWave.GetEnemyPrefab(0), randomSpawnPoint.position, Quaternion.identity);
+                    enemy = Instantiate(currentWave.GetEnemyPrefab(0), randomEnemySpawnPoint.position, Quaternion.identity);
                     Debug.Log("Enemy Alive");
-                    alivelist.Add(enemy);
+                    aliveList.Add(enemy);
                     yield return new WaitForSeconds(wave.GetSpawnTime());
 
                 }
             }
-            while (alivelist.Count > 50)
+            for (int j = 0; j < currentWave.GetWeaponCount(); j++)
+            {
+                var randomWeaponSpawnPoint = WeaponSpawnPoints[Random.Range(0, WeaponSpawnPoints.Length - 1)];
+                GameObject weapon;
+                weapon = Instantiate(currentWave.GetWeaponPrefab(0), randomWeaponSpawnPoint.position, Quaternion.identity);
+                weaponList.Add(weapon);
+                Debug.Log("Weapon in scene and weapon spawn points");
+            }
+            while (aliveList.Count > 25)
             {
                 canSpawn = false;
             }
-            while (alivelist.Count > 0)
+            while (aliveList.Count > 0)
             {
                 yield return null;
             }
